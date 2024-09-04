3d0df65c2200616a05d8a0a9a57a4470b20e9d26
John Makasi <makasijohn21@gmail.com>
2024-09-04 18:15:11 +0200

    Installation de Npm, TailwindCSS et de Flowbite

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rotana Hotel</title>
+    <link href="./css/output.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="text-center text-blue-800 text-4xl">Bienvenue au site officiel de l'hotel Rotana</h1>
+    <script src="./node_modules/flowbite/dist/flowbite.min.js"></script>
+</body>
+</html>
