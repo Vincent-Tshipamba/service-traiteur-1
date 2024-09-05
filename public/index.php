b7cc29ea05e580d9368260e1127e2ebf4b1f6eb2
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-09-05 17:04:18 +0100

    Feat: ajout de la barre de navigation et de la premiere section de la page d'accueil. Egalement la redirection des requetes vers le fichier index.php en definissant des regles de reecritures dans le fichier .htaccess

--- public/index.php
+++ public/index.php
@@ -1,22 +1,46 @@
-<?php 
-    require('../config/database.php');
+<?php
+require('../config/database.php');
 
-    $database = new Database;
+$database = new Database;
 
-    $connexion = $database->getConnection();
+$connexion = $database->getConnection();
 
+// Récupérer l'URI après le domaine
+$request = $_SERVER['REQUEST_URI'];
+
+// Retirer les paramètres GET s'il y en a
+$request = strtok($request, '?');
+
+// Définir les routes
+switch ($request) {
+    case '/':
+        require 'index.php';
+        break;
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rotana Hotel</title>
     <link href="./css/output.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1 class="text-center text-blue-800 text-4xl">Bienvenue au site officiel de l'hotel Rotana</h1>
+    <?php include_once('./partials/en-tete.php'); ?>
+
+
+    <main class="">
+        <?php include_once('./partials/premiereSection.php'); ?>
+    </main>
+
+
+
     <script src="./node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
+
 </html>
