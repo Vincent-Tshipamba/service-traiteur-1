9fd0d04fd84807523f5dd65ed7ff46efac25001d
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-09-18 09:30:55 +0200

    Feat: fixing the navbar cta

--- public/parties/en-tete.php
+++ public/parties/en-tete.php
@@ -18,9 +18,9 @@
             </div>
             <div class="flex items-center justify-end gap-3">
                 <a class="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                    href="/login">Se connecter</a>
+                    href="#contact">Nous contacter</a>
                 <a class="inline-flex items-center justify-center rounded-xl bg-yellow-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
-                    href="/login">S'inscrire</a>
+                    href="#menus">Explorer</a>
             </div>
         </div>
     </div>
